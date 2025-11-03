477a72cde9e59e329f3e3e4c86ad11d2a916c3fc
Edoardo Albanese <edoardo.albanese.s25@liceoviconapoli.it>
2025-11-03 13:06:58 +0100

    dizionario

---
+++ Recap/recap3.py
@@ -0,0 +1,13 @@
+lista = list()
+lista.append("banana")
+
+
+cartaidentita = {
+"Nome" : "Edoardo",
+"cognome" : "albanese",
+"età": 16,
+"codice fiscale": "jnainpn"
+}
+
+
+print(cartaidentita.get("Nome"))
