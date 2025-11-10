bbf9134f33472fbf25f17d6cc0e9b48675dc758a
Edoardo Albanese <edoardo.albanese.s25@liceoviconapoli.it>
2025-11-10 13:11:02 +0100

    modules

--- ImportCsv/main.py
+++ ImportCsv/main.py
@@ -1,11 +1,11 @@
 import csv
 import os
 
-elements = os.listdir("../ImportCsv")
+elements = os.listdir("ImportCsv")
 print(elements)
 print(elements[0])
 
-path = "../ImportCsv" + elements[0]
+path = "ImportCsv/" + elements[0]
 
 with open(path, "r") as file:
     reader = csv.reader(file)
