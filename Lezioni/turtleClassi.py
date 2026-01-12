dc515b1ca61a87b2e88dcf17fee10feae26ad93c
Edoardo Albanese <edoardo.albanese.s25@liceoviconapoli.it>
2026-01-12 13:10:26 +0100

    turtle rombi

--- Lezioni/turtleClassi.py
+++ Lezioni/turtleClassi.py
@@ -1,18 +1,35 @@
 import turtle
+import random
 
 screen = turtle.Screen()
-screen.setup(670, 420)
-screen.title("Esperimento Classi")
-screen.colormode(255)
-screen.bgcolor(
-    (53, 12, 67)
-)
+screen.setup(420, 380)
+gurt = turtle.Turtle()
 
-gurt = turtle.Turtle("turtle")
-gurt.color("Yellow")
+minside = 10
+maxside = 200
+amount = random.randint(0, 20)
 
-for i in range(4):
-    gurt.forward(100)
-    gurt.right(30)
+def rhombus(posx : int, posy : int):
+    gurt.goto(posx, posy)
+    gurt.pendown()
+    gurt.setheading(random.randint(0, 360))
+    side_a = random.randint(minside, maxside)
+    side_b = random.randint(minside, maxside)
+    sides = [side_a, side_b]
+    angle_a = random.randint(10, 170)
+    angle_b = 180 - angle_a
+    angles = [angle_a, angle_b]
+    for i in range(4):
+        gurt.forward(sides[i % 2])
+        gurt.right(angles[i % 2])
+    gurt.penup()
 
-input("Enter to end")
+gurt.penup()
+for i in range(amount):
+    posx = random.randint(0, int(screen.xscale))
+    posy = random.randint(0, int(screen.yscale))
+    print (posx, posy)
+    rhombus(posx, posy)
+
+
+turtle.done()
