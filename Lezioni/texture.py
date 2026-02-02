f3861878528deb5e431bd5552aa4987391950de6
Edoardo Albanese <edoardo.albanese.s25@liceoviconapoli.it>
2026-02-02 13:09:41 +0100

    texture 2

--- Lezioni/texture.py
+++ Lezioni/texture.py
@@ -5,28 +5,29 @@ turt = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(1.0,1.0)
 
-rows = 5
-columns = 5
-padding = 2
+rows = 2
+columns = 2
+padding = 50
+side_coefficient = 1 + 2/3
 
 width = screen.window_width()
 height = screen.window_height()
 
 def polygon(sides : int, size : int):
     turt.pendown()
-    turt.setheading(random.randint(0,360))
+    #turt.setheading(random.randint(0,30))
     for i in range(sides):
         turt.forward(size)
         turt.right(360/sides)
     turt.penup()
 
 def draw_texture():
-    for i in range(columns):
-        ypos = (height / columns) * i
-        for j in range(rows):
-            xpos = (width/rows) * j
-            sides = random.randint(3, 20)
-            side_lenghth = ((width/rows) - padding) / sides
+    for i in range(rows):
+        ypos = height/2 - (height/rows * i)
+        for j in range(columns):
+            sides = random.randint(3, 10)
+            side_lenghth = (width/columns) / sides * side_coefficient - padding
+            xpos = - width/2 + width/columns * j
             turt.goto(xpos, ypos)
             polygon(sides, side_lenghth)
 
